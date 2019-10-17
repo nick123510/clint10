122e9ee112dbdcff6b93492f2c79a7560be1afa3
CJ <charles.sweatt>
2019-10-17 14:56:07 -0700

    Fixed ChangeLevelOnCollide and created Circle.png

--- FlatPlatformer_students_final/Assets/Flat Platformer Template/Scripts/ChangeLevelOnCollide.cs
+++ FlatPlatformer_students_final/Assets/Flat Platformer Template/Scripts/ChangeLevelOnCollide.cs
@@ -17,40 +17,24 @@ public class ChangeLevelOnCollide : MonoBehaviour
     public UnityEvent OnHit;
 
 
-    void Start()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        offset = new Vector2(Mathf.Abs(offset.x), offset.y);
-        FindPlayer();
-        _cam = gameObject.GetComponent<Camera>();
-    }
-
-    public void FindPlayer()
-    {
-        lastX = Mathf.RoundToInt(_target.position.x);
-        transform.position = new Vector3(_target.position.x + offset.x, _target.position.y + offset.y, transform.position.z);
-    }
-
-    void FixedUpdate()
-    {
-        if (_target)
+        if (collision.gameObject.CompareTag("Player"))
         {
-            int currentX = Mathf.RoundToInt(_target.position.x);
-            if (currentX > lastX) faceLeft = false; else if (currentX < lastX) faceLeft = true;
-            lastX = Mathf.RoundToInt(_target.position.x);
-
-            Vector3 target;
-            if (faceLeft)
-            {
-                target = new Vector3(_target.position.x - offset.x, _target.position.y + offset.y + dynamicSpeed, transform.position.z);
-            }
-            else
-            {
-                target = new Vector3(_target.position.x + offset.x, _target.position.y + offset.y + dynamicSpeed, transform.position.z);
-            }
-            Vector3 currentPosition = Vector3.Lerp(transform.position, target, damping * Time.deltaTime);
-            transform.position = currentPosition;
+            //run any extra effects
+            OnHit.Invoke();
+            StartCoroutine(ChangeSceneAfterDelay());
         }
     }
+
+    IEnumerator ChangeSceneAfterDelay()
+    {
+        //pause for the delay
+        yield return new WaitForSeconds(changeDelay);
+        //finally change level
+        SceneManager.LoadScene(NextScene);
+    }
+
 
 
 
